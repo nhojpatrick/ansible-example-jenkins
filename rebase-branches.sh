d7ef9aae39aee5f1f4e93d0c81c209e646d2d0cf
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-02-10 22:26:47 +0000

    Jenkins v2.277.x archive

--- rebase-branches.sh
+++ rebase-branches.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 git checkout main     && git pull            && git push -f && \
-git checkout v2.277.x && git rebase main     && git push -f && \
-git checkout v2.289.x && git rebase v2.277.x && git push -f && \
+git checkout v2.289.x && git rebase main     && git push -f && \
 git checkout v2.303.x && git rebase v2.289.x && git push -f && \
 git checkout v2.319.x && git rebase v2.303.x && git push -f && \
 git checkout v2.332.x && git rebase v2.319.x && git push -f && \
